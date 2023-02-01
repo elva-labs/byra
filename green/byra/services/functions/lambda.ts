9a01ebb851762fcfef5d67dd9939115f41f1d374
Sebastian Bille <sebastianbille@outlook.com>
2023-02-01 17:03:59 +0100

    wip green

--- green/byra/services/functions/lambda.ts
+++ green/byra/services/functions/lambda.ts
@@ -1,9 +1,16 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import middy from '@middy/core';
+import { logMetrics, Metrics, MetricUnits } from "@aws-lambda-powertools/metrics";
 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+const metrics = new Metrics({ namespace: 'elva-labs', serviceName: 'byra' });
+
+export const lambdaHandler: APIGatewayProxyHandlerV2 = async (event) => {
+  metrics.addMetric('beerWeight', MetricUnits.Count, 3245);
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
-  };
+    body: JSON.stringify({ message: 'success' }),
+  }
 };
+
+export const handler = middy(lambdaHandler)
+    .use(logMetrics(metrics));
--- green/byra/stacks/MyStack.ts
+++ green/byra/stacks/MyStack.ts
@@ -1,4 +1,18 @@
 import { StackContext, Api } from "@serverless-stack/resources";
+import { Metric } from "aws-cdk-lib/aws-cloudwatch";
 
 export function MyStack({ stack }: StackContext) {
+  const metric = new Metric({
+    namespace: 'elva-labs',
+    metricName: 'beerWeight',
+  });
+
+  const alarm = metric.createAlarm(stack, 'BeerAlarm', {
+    alarmName: 'BeerAlarm',
+    alarmDescription: 'Alarm when beer weight is too low',
+    // comparisonOperator: 'LessThanThreshold',
+    evaluationPeriods: 1,
+    threshold: 1000,
+    // treatMissingData: 'notBreaching',
+  });
 }
